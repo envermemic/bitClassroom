c3bbe4e4e85ce0d652b09451afe59f5af7e66527
nidalBITCamp <nidal.salkic@bitcamp.ba>
2015-10-18 16:34:18 +0200

    initial commit

--- app/controllers/users/AdminController.java
+++ app/controllers/users/AdminController.java
@@ -4,10 +4,7 @@ import com.avaje.ebean.Ebean;
 import helpers.Authorization;
 import helpers.SessionHelper;
 import it.innove.play.pdf.PdfGenerator;
-import models.ErrorLog;
-import models.Faq;
-import models.Image;
-import models.PrivateMessage;
+import models.*;
 import models.course.Course;
 import models.course.CourseUser;
 import models.report.*;
@@ -37,16 +34,23 @@ import views.html.admins.adduser;
 import views.html.admins.newTableWeekly;
 import views.html.admins.weeklypdf;
 import views.html.admins.openReports;
+import views.html.calendar.calendar;
+import views.html.calendar.formEdit;
+import views.html.calendar.formNew;
+import views.html.calendar.list;
+
 import views.html.admins.setingsweeklyreport;
 import views.html.admins.dailypdf;
 import views.html.admins.pdfopenreport;
 import javax.inject.Inject;
 import javax.persistence.PersistenceException;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.UUID;
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import play.data.Form;
 
 
 /**
@@ -59,6 +63,8 @@ public class AdminController extends Controller {
     public PdfGenerator pdfGenerator;
 
     private final Form<User> userForm = Form.form(User.class);
+
+    final static Form<Event> eventForm = Form.form(Event.class);
 
     private final Form<Field> fieldForm = Form.form(Field.class);
 
@@ -862,6 +868,213 @@ public class AdminController extends Controller {
 
         return redirect("/faq");
     }
+
+    /**
+     * Checks if events ends the same day which starts
+     * @param start Date
+     * @param end Date
+     * @return Boolean: True if ends same day
+     */
+    private static Boolean endsSameDay(Date start, Date end){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        return dateFormat.format(start).equals(dateFormat.format(end));
+    }
+
+
+    /**
+     * Returns list of events for calendar view
+     * @param start Long Timestamp of current view start
+     * @param end Long Timestamp of current view end
+     * @return Result
+     */
+    public static Result json(Long start, Long end) {
+
+        Date startDate = new Date(start*1000);
+        Date endDate = new Date(end*1000);
+
+        List<Event> resultList = Event.findInDateRange(startDate, endDate);
+        ArrayList<Map<Object, Serializable>> allEvents = new ArrayList<Map<Object, Serializable>>();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        for (Event event : resultList) {
+            Map<Object, Serializable> eventRemapped = new HashMap<Object, Serializable>();
+            eventRemapped.put("id", event.id);
+            eventRemapped.put("title", event.title);
+            eventRemapped.put("start", df.format(event.start));
+            eventRemapped.put("end", df.format(event.end));
+            eventRemapped.put("allDay", event.allDay);
+            eventRemapped.put("url", controllers.users.routes.AdminController.edit(event.id).toString());
+
+            allEvents.add(eventRemapped);
+        }
+        return ok(play.libs.Json.toJson(allEvents));
+    }
+
+
+    /**
+     * Displays full calendar
+     * @return Result
+     */
+    public static Result calendar() {
+        return ok(calendar.render("Title of this calendar..."));
+    }
+
+
+    /**
+     * List of events in table view
+     * @return Result
+     */
+    public static Result listEvents() {
+        List<Event> events = Event.find.order().desc("start").findList();
+        return ok(list.render(events));
+    }
+
+
+    /**
+     * Displays blank form
+     * @return Result
+     */
+    public static Result blank() {
+        return ok(formNew.render(eventForm));
+    }
+
+
+    /**
+     * Save new event in DB (a.k.a. submit action in other examples)
+     * @return Result
+     */
+    public static Result add() {
+        Form<Event> eventForm = Form.form(Event.class).bindFromRequest();
+        if (eventForm.hasErrors()) {
+            return badRequest(formNew.render(eventForm));
+        }
+
+        Event newEvent = eventForm.get();
+
+        newEvent.allDay = newEvent.allDay != null;
+        if (newEvent.end == null) {
+            newEvent.end = new DateTime(newEvent.start).plusMinutes(30).toDate();
+        }
+        newEvent.endsSameDay = endsSameDay(newEvent.start, newEvent.end);
+        newEvent.save();
+        return redirect("/events");
+    }
+
+
+    /**
+     * Dislays form for editing existing event
+     * @param id Long
+     * @return Result
+     */
+    public static Result edit(Long id) {
+        Event event = Event.find.byId(id);
+        Form<Event> eventForm = Form.form(Event.class).fill(event);
+        return ok(formEdit.render(id, eventForm, event));
+    }
+
+
+    /**
+     * Save new event in DB (a.k.a. submit action in other examples)
+     * @param id Long
+     * @return Result
+     */
+    public static Result update(Long id) {
+        Form<Event> eventForm = Form.form(Event.class).bindFromRequest();
+        if (eventForm.hasErrors()) {
+            return badRequest(formEdit.render(id, eventForm, Event.find.byId(id)));
+        }
+        Event updatedEvent = eventForm.get();
+        updatedEvent.allDay = updatedEvent.allDay != null;
+        if (updatedEvent.end == null) {
+            updatedEvent.end = new DateTime(updatedEvent.start).plusMinutes(30).toDate();
+        }
+        updatedEvent.endsSameDay = endsSameDay(updatedEvent.start, updatedEvent.end);
+        updatedEvent.update(id);
+        return redirect("/events");
+    }
+
+
+    /**
+     * Deletes event
+     * @param id Long
+     * @return Result
+     */
+    public static Result delete(Long id) {
+        Event.find.ref(id).delete();
+        return redirect("/events");
+    }
+
+
+    /**
+     * Adds event after clicking on calendar
+     * @return Result
+     */
+    public static Result addByAjax() {
+        Form<Event> eventForm = Form.form(Event.class).bindFromRequest();
+        Event newEvent = eventForm.get();
+        newEvent.endsSameDay = endsSameDay(newEvent.start, newEvent.end);
+        newEvent.save();
+
+        if (eventForm.hasErrors()){
+            return badRequest("There was some errors in form");
+        }
+
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("id", newEvent.id.toString());
+        result.put("url", routes.AdminController.edit(newEvent.id).toString());
+
+        return ok(play.libs.Json.toJson(result));
+    }
+
+
+    /**
+     * Saves in DB date changed by event drag
+     * @return Result
+     */
+    public static Result move() {
+
+        Long id = Long.valueOf(Form.form().bindFromRequest().get("id"));
+        int dayDelta = Integer.parseInt(Form.form().bindFromRequest().get("dayDelta"));
+        int minuteDelta = Integer.parseInt(Form.form().bindFromRequest().get("minuteDelta"));
+
+        Event event = Event.find.byId(id);
+        event.start = new DateTime(event.start).plusDays(dayDelta).plusMinutes(minuteDelta).toDate();
+        event.end = new DateTime(event.end).plusDays(dayDelta).plusMinutes(minuteDelta).toDate();
+        event.allDay = Boolean.valueOf(Form.form().bindFromRequest().get("allDay"));
+        event.endsSameDay = endsSameDay(event.start, event.end);
+        event.update();
+
+//        if (thereIsSomeError){
+//            return badRequest("You can not move this event!");
+//        }
+
+        return ok("changed");
+    }
+
+    /**
+     * Saves in DB end date changed by event resize
+     * @return Result
+     */
+    public static Result resize()
+    {
+
+        Long id = Long.valueOf(Form.form().bindFromRequest().get("id"));
+        int dayDelta = Integer.parseInt(Form.form().bindFromRequest().get("dayDelta"));
+        int minuteDelta = Integer.parseInt(Form.form().bindFromRequest().get("minuteDelta"));
+
+        Event event = Event.find.byId(id);
+        event.end = new DateTime(event.end).plusDays(dayDelta).plusMinutes(minuteDelta).toDate();
+        event.endsSameDay = endsSameDay(event.start, event.end);
+        event.update();
+
+//        if (thereIsSomeError){
+//            return badRequest("You can not resize this event!");
+//        }
+
+        return ok("changed");
+    }
+
+
 //======================================================================================================================
 // Refactoring reports
 
@@ -967,10 +1180,12 @@ public class AdminController extends Controller {
     public Result pdfDailyReports(Long id) {
         return pdfGenerator.ok(pdfopenreport.render(DailyReport.findDailyReportById(id), ReportField.getFinder().all()), Configuration.root().getString("application.host"));
     }
+
+
+
 //    Radnja u pripremi
 //    public Result pdfWeeklyReports() {
 //        return pdfGenerator.ok();
 //    }
-
 
 }
---
+++ app/models/Event.java
@@ -0,0 +1,72 @@
+package models;
+
+import com.avaje.ebean.Expr;
+import com.avaje.ebean.Model;
+import play.data.format.Formats;
+import play.data.validation.Constraints;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by NN on 18.10.2015.
+ */
+
+@Entity
+public class Event extends Model {
+
+    @Id
+    public Long id;
+
+
+    @Constraints.Required
+    public String title;
+    public Boolean allDay;
+
+    @Constraints.Required
+    @Formats.DateTime(pattern = "dd.MM.yyyy HH:mm")
+    public Date start = new Date();
+
+    @Formats.DateTime(pattern = "dd.MM.yyyy HH:mm")
+    public Date end = new Date();
+
+    public Boolean endsSameDay;
+
+
+    public static Finder<Long,Event> find = new Finder<Long,Event>(Long.class, Event.class);
+
+    public Event(String title, Date start, Date end, Boolean allDay) {
+        this.title = title;
+        this.start = start;
+        this.end = end;
+        this.allDay = allDay;
+
+    }
+
+
+    public static List<Event> findInDateRange(Date start, Date end) {
+
+
+        return find.where().or(
+                Expr.and(
+                        Expr.lt("start", start),
+                        Expr.gt("end", end)
+                ),
+                Expr.or(
+                        Expr.and(
+                                Expr.gt("start", start),
+                                Expr.lt("start", end)
+                        ),
+                        Expr.and(
+                                Expr.gt("end", start),
+                                Expr.lt("end", end)
+                        )
+                )
+        ).findList();
+    }
+
+
+
+}
